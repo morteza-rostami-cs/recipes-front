4119cfd86920e4cbe8f969a1b3c1836199c363b4
morteza <morteza.rostami.cs@gmail.com>
2025-11-20 13:13:46 -0800

    chore: add domain to vite.config

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,10 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  preview: {
+    host: true, // allows network access
+    port: 4173, // match your docker port
+    strictPort: true,
+    allowedHosts: ["recipes.mortteza.site", "localhost"], // add your host here
+  },
 });
